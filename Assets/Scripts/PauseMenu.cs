a6f9729489b3badcdce90622e2652b8e286d699d
dbhan165 <dinhbaohan16052004@gmail.com>
2025-11-13 16:12:23 +0700

    pause menu

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,42 +1,53 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PauseMenu : MonoBehaviour
 {
     public static bool GameIsPaused = false;
     public GameObject pauseMenuUI;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        InputSystem.Update();
-        if (Keyboard.current.escapeKey.wasPressedThisFrame)
-        {
-            if (GameIsPaused)
-            {
-                Resume();
-            }
-            else
-            {
-                Pause();
-            }
-        }
-    }
-    public void Resume()
-    {
+        // Khi bắt đầu game, đảm bảo đang chạy bình thường
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
         GameIsPaused = false;
     }
+
+    void Update()
+    {
+        // Nếu chưa có bàn phím thì bỏ qua
+        if (Keyboard.current == null)
+            return;
+
+        // Cập nhật Input System để nhận phím cả khi Time.timeScale = 0
+        InputSystem.Update();
+
+        // Bấm ESC để bật/tắt menu
+        if (Keyboard.current.escapeKey.wasPressedThisFrame)
+        {
+            Debug.Log("ESC pressed!"); // để kiểm tra xem có bắt được phím không
+
+            if (GameIsPaused)
+                Resume();
+            else
+                Pause();
+        }
+    }
+
+    void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        Debug.Log("Game resumed");
+    }
+
     void Pause()
     {
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         GameIsPaused = true;
+        Debug.Log("Game paused");
     }
 }
