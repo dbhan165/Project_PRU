85e456e53a2f36243e02cfd76ae9c630efcfaf71
dbhan165 <dinhbaohan16052004@gmail.com>
2025-11-14 14:48:51 +0700

    thêm child cho player khi giết enemy

---
+++ Assets/Scripts/EagleFollow.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class EagleFollower : MonoBehaviour
+{
+    private Transform player;
+    public Vector3 offset = new Vector3(-1.5f, 1.5f, 0f);
+    public float followSpeed = 5f;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void Update()
+    {
+        if (player == null) return;
+
+        Vector3 targetPos = player.position + offset;
+
+        transform.position = Vector3.Lerp(transform.position, targetPos, followSpeed * Time.deltaTime);
+    }
+}
